0e93b4fec94aba076932b26354ab55b733ccec3f
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-04-18 00:09:46 -0500

    Add database functionality - Models, migrations

--- wish_friend_list/base/views.py
+++ wish_friend_list/base/views.py
@@ -2,25 +2,18 @@ import imp
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Cumpleaños Sofi'},
-    {'id': 2, 'name': 'Boda S&J'},
-    {'id': 3, 'name': 'Parrillada José'},
-
-]
+# rooms = [{'id': 1, 'name': 'Cumpleaños Sofi'}, {'id': 2, 'name': 'Boda S&J'}, {'id': 3, 'name':'Parrillada José'}]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'room.html', context)
